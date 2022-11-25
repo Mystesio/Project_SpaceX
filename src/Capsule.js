8a0fc64a2c79958580f07692249f5c0a948d13b5
guillaumeTT <guillaume.toutain76@gmail.com>
2022-11-25 12:41:11 +0100

    Ajout fichier functions.js et optimisation fichiers Launch.js et Capsule.js

--- src/Capsule.js
+++ src/Capsule.js
@@ -1,36 +1,17 @@
 import React from 'react';
 import './App.css';
+import affichageElements from './functions';
 
 class Capsule extends React.Component {
     render() {
         var tab = ["capsule_serial","capsule_id", "details"];
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-          
-          fetch("https://api.spacexdata.com/v3/capsules", requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                        for (let i = 0; i < result.length; i++){ 
-                            for (let j = 0; j < tab.length; j++){ 
-                            document.getElementById("capsule").innerHTML += tab[j] + " -> " + JSON.parse(result)[i][tab[j]] + " "
-                            }
-                            document.getElementById("capsule").innerHTML +=  "<br />"
-                        }
-                    }
-                )
-            .catch(error => console.log('error', error));
-        
+        affichageElements(tab, "capsule", "https://api.spacexdata.com/v3/capsules");
         return (
-
-        <div>
-        <h1>Les capsules</h1>
-            <div id='capsule'>
+            <div>
+                <h1>Les capsules</h1>
+                <div id='capsule'></div>
             </div>
-        </div>
         )
-        
     }
 }
 export default Capsule;
--- src/Launch.js
+++ src/Launch.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import affichageElements from './functions';
 
 class Launch extends React.Component {
     render() {
         var tab = ["mission_name","launch_year", "launch_success"];
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-          
-          fetch("https://api.spacexdata.com/v3/launches", requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                        for (let i = 0; i < result.length; i++){ 
-                            for (let j = 0; j < tab.length; j++){ 
-                            document.getElementById("launch").innerHTML += tab[j] + " -> " + JSON.parse(result)[i][tab[j]] + " "
-                            }
-                            document.getElementById("launch").innerHTML +=  "<br />"
-                        }
-                    }
-                )
-            .catch(error => console.log('error', error));
-        
+        affichageElements(tab, "launch", "https://api.spacexdata.com/v3/launches");
         return (
             <div>
-            <h1>Les lancement</h1>
-            <div id='launch'>
-
-            </div>
-
-            
+                <h1>Les lancement</h1>
+                <div id='launch'></div>
             </div>
         )
-        
     }
 }
 export default Launch;
